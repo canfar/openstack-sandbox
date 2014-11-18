b3a24b0ae1711d5c9688c3e356dbf06d099f79e4
Sebastien Fabbro <sfabbro@uvic.ca>
2014-11-18 09:29:23 -0800

    condor config file fixes, now executing the functions

--- vm_config/cloud_scheduler_setup.bash
+++ vm_config/cloud_scheduler_setup.bash
@@ -2,12 +2,12 @@
 # Shell script for installing and configuring Condor to enable dynamically
 
 EXEC_NAME=$(basename $0 .${0##*.})
-EPHEMERAL_DEVICE=/dev/disk/by-label/ephemeral0
-STAGING_DIR=${TMPDIR:-/staging}
 
-CS_CENTRAL_MANAGER=canfarhead
+EPHEMERAL_DEVICE=/dev/disk/by-label/ephemeral0
+EPHEMERAL_DIR=/staging
+
+CS_CENTRAL_MANAGER=queue
 CS_CENTRAL_MANAGER_IP=192.168.0.3
-CS_CONDOR_CONFIG="/etc/condor/config.d/cloud_scheduler"
 CS_VM_TYPE=$(hostname)
 
 msg() {
@@ -21,29 +21,37 @@ die() {
 
 # mount ephemeral disk as scratch disk
 # would need something more portable than /proc grepping
-cs_mount_scratch() {
-    grep -q ${STAGING_DIR} /proc/mounts && msg "scratch already directory mounted" && return 0
-    msg "mount ephemeral disk at ${STAGING_DIR}..."
-    mkdir -p ${STAGING_DIR}
+cs_mount_ephemeral() {
+    grep -q ${EPHEMERAL_DIR} /proc/mounts && msg "scratch already directory mounted" && return 0
+    msg "mount ephemeral disk at ${EPHEMERAL_DIR}..."
+    mkdir -p ${EPHEMERAL_DIR}
     if [[ -b ${EPHEMERAL_DEVICE} ]] ; then
-	mount -o defaults ${EPHEMERAL_DEVICE} ${STAGING_DIR} || die "failed to mount ${EPHEMERAL_DEVICE} at ${STAGING_DIR}"
-	msg "scratch directory mounted on ${STAGING_DIR}"
+	mount -o defaults ${EPHEMERAL_DEVICE} ${EPHEMERAL_DIR} || die "failed to mount ${EPHEMERAL_DEVICE} at ${EPHEMERAL_DIR}"
+	msg "ephemeral directory mounted on ${EPHEMERAL_DIR}"
     else
 	msg "partition labeled 'ephemeral0' does not exist"
-	msg "${STAGING_DIR} will not be configured"
+	msg "${EPHEMERAL_DIR} will not be configured"
     fi
+    chmod ugo+rwxt ${EPHEMERAL_DIR}
 }
 
-# set up condor ccb if only private networking is available
 cs_setup_etc_hosts() {
+    # set up condor ccb if only private networking is available
     ifconfig | grep "inet addr" | egrep -v "addr:127.|addr:192.|addr:172.|addr:10." > /dev/null && return 0
     # ip are local
-    local addstr="# Added for cloud_scheduler to connect to condor CCB"       
+    local addstr="# Added for cloud_scheduler to connect to condor CCB"
     local ip=$(ifconfig eth0 | grep -oP '(?<=inet addr:)[0-9.]*')
     if grep -q "${addstr}" /etc/hosts ; then
 	sed -i -e "s:.*\(${addr}\):${ip} ${HOSTNAME} \1:" /etc/hosts
     else
 	echo >> /etc/hosts "${ip} ${HOSTNAME} ${addstr}"
+    fi
+    # now add central manager host in case of no dns
+    local addstr="# Added for cloud_scheduler to connect to central manager"
+    if grep -q "${addstr}" /etc/hosts ; then
+	sed -i -e "s:.*\(${addr}\):${CS_CENTRAL_MANAGER_IP} ${CS_CENTRAL_MANAGER} \1:" /etc/hosts
+    else
+	echo >> /etc/hosts "${CS_CENTRAL_MANAGER_IP} ${CS_CENTRAL_MANAGER} ${addstr}"
     fi
 }
 
@@ -85,10 +93,19 @@ cs_condor_install() {
 # configure condor for cloud scheduler
 cs_configure_condor() {
     msg "updating condor config"
-    cat > ${CS_CONDOR_CONFIG} <<-EOF
+    type -P condor_config_val || die "condor does not seem to be installed"
+    local condorconfig="$(condor_config_val LOCAL_CONFIG_DIR)"
+    if [[ -n ${condorconfig} ]]; then
+	mkdir -p ${condorconfig}
+	condorconfig="${condorconfig}/cloud_scheduler"
+    else
+	condorconfig="$(condor_config_val LOCAL_CONFIG_FILE)"
+	[[ -n ${condorconfig} ]] || die "condor configuration file '${condorconfig}' is undefined"
+    fi
+    cat >> ${condorfile} <<-EOF
 	#########################################################
 	# Automatically added for cloud_scheduler by ${EXEC_NAME}
-	EXECUTE = ${CS_STAGING_DIR}
+	EXECUTE = ${CS_EPHEMERAL_DIR}
 	CONDOR_HOST = ${CS_CENTRAL_MANAGER}
 	HOSTALLOW_WRITE = \$(FULL_HOSTNAME), \$(CONDOR_HOST), \$(IP_ADDRESS)
 	ALLOW_WRITE = \$(FULL_HOSTNAME), \$(CONDOR_HOST), \$(IP_ADDRESS)
@@ -109,7 +126,12 @@ cs_configure_condor() {
 	######################################################
     EOF
     echo "${CS_CENTRAL_MANAGER}" > /etc/condor/central_manager
-    chown condor:condor ${STAGING_DIR}
+    chown condor:condor ${EPHEMERAL_DIR}
     msg "restart condor services to include configuration changes"
     service condor restart
 }
+
+cs_mount_ephemeral
+cs_install_condor
+cs_configure_condor
+cs_setup_etc_hosts
