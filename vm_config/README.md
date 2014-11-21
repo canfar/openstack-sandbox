9afd0970263436677603c6e3046877d786855f4a
Ed Chapin <Ed.Chapin@nrc-cnrc.gc.ca>
2014-11-21 15:03:44 -0800

    major cleanup of startup scripts
    We now have a base config script cloud_scheduler_setup.bash.
    A wrapper cloud-config script is generated with make_cloud_config.
    The wrapper both mounts ephemeral partitions, and also installs
    condor / updates cloud scheduler config.

--- vm_config/README.md
+++ vm_config/README.md
@@ -1,3 +1,21 @@
-# CANFAR Migration to OpenStack - cloud-init configuration
+# CANFAR Migration to OpenStack - batch jobs with VMs
 
-This directory contains files to facilitate dynamic configuration of VMs using cloud-init at boot time.
+This directory contains scripts relevant to the configuration of user
+VMs for use with Cloud Scheduler, and job submission.
+
+## First time configuration
+
+The only initial configuration that is recommended is the installation
+of Condor. ```cloud_scheduler_setup.bash``` may be supplied as **user data** when intantiating a new VM for the first time, or by executing it (with no arguments) as **root** from within the running VM. If launching an instance from the dashboard, the file may be copy-and-pasted into the **Post-Creation** tab, or even more simply using an ```#include``` with a link to the script in the GitHub repo, e.g.:
+```
+#include
+https://raw.githubusercontent.com/canfar/openstack-sandbox/master/vm_config/cloud_scheduler_setup.bash
+```
+
+## Job submission
+
+When a user launches a job, their VM will need to be **shared** with the batch processing tenant. ```canfar_submit``` is a Python script that first shares the VM and then calls ```condor_submit```.
+
+## Cloud Scheduler
+
+The cloud scheduler configuration may be updated dynamically by injecting a configuration script into the VM. We use a **cloud-config** script as it provides a clean mechanism for mounting ephemeral partitions, and it also wraps the ```cloud_scheduler_setup.bash``` (although now providing the ```--update-cloud-scheduler``` option to set, among other things, the central manager). The cloud-config script itself is a YAML file, and is not explicitly included in this repo. Instead, it is generated from ```cloud_scheduler_setup.bash``` by running ```make_cloud_config```, which produces ```cloud_config.yml```. It is this last file which should be supplied for ```VMAMIData``` in the job submission file.
--- vm_config/cloud_scheduler_setup.bash
+++ vm_config/cloud_scheduler_setup.bash
@@ -5,8 +5,9 @@ EXEC_NAME=$(basename $0 .${0##*.})
 EXEC_VERSION=0.1_beta
 
 EPHEMERAL_DIR="/ephemeral"
-EPHEMERAL_DEVICE="/dev/disk/by-label/ephemeral0"
+
 CM_HOST_NAME="batch"
+
 # need to specify local ip because no local dns on nefos
 CM_HOST_IP="192.168.0.3"
 
@@ -26,6 +27,7 @@ Configure HTCondor for cloud-scheduler on VM execution hosts
   -c, --central-manager     set the central manager hostname (default: ${CM_HOST_NAME})
   -i, --central-manager-ip  set the central manager local IP (default: ${CM_HOST_IP})
   -e, --ephemeral-dir       scratch directory where condor will execute jobs (default: ${EPHEMERAL_DIR})
+  -u, --update-cloud-scheduler update cloud scheduler configuration if set
   -h, --help                display help and exit
   -v, --version             output version information and exit
 "
@@ -70,21 +72,6 @@ condor_install() {
    else
        die "unable to detect distribution type"
    fi
-}
-
-# mount ephemeral partition
-# would need something more portable than /proc grepping
-cs_mount_ephemeral() {
-    grep -q ${EPHEMERAL_DIR} /proc/mounts && msg "scratch already directory mounted" && return 0
-    msg "mounting ephemeral disk on ${EPHEMERAL_DIR}"
-    mkdir -p ${EPHEMERAL_DIR}
-    if [[ -b ${EPHEMERAL_DEVICE} ]]; then
-        mount -o defaults ${EPHEMERAL_DEVICE} ${EPHEMERAL_DIR} \
-	    || die "failed to mount ${EPHEMERAL_DEVICE} at ${EPHEMERAL_DIR}"
-    else
-        msg "partition labeled 'ephemeral0' does not exist."
-        msg "${EPHEMERAL_DIR} will not be configured."
-    fi
 }
 
 # configure condor for cloud scheduler
@@ -153,15 +140,18 @@ cs_setup_etc_hosts() {
 
 # Store all options
 OPTS=$(getopt \
-    -o c:e:i:hv \
+    -o c:i:e:uhv \
     -l central-manager: \
     -l central-manager-ip: \
     -l ephemeral-dir: \
+    -l update-cloud-scheduler \
     -l help \
     -l version \
     -- "$@")
 
 eval set -- "${OPTS}"
+
+UPDATE_CS=false
 
 # Process options
 while true; do
@@ -169,6 +159,7 @@ while true; do
 	-c | --central-manager) CM_HOST_NAME=${2##=}; shift ;;
 	-i | --central-manager-ip) CM_HOST_IP=${2##=}; shift ;;
 	-e | --ephemeral-dir) EPHEMERAL_DIR=${2##=}; shift ;;
+        -u | --update-cloud-scheduler) UPDATE_CS=true; shift ;;
 	-h | --help) usage ;;
 	-V | --version) echo ${EXEC_VERSION}; exit ;;
 	--)  shift; break ;; # no more options
@@ -177,7 +168,10 @@ while true; do
     shift
 done
 
+export PATH=/sbin:/usr/sbin:${PATH}
+
 condor_install
-cs_mount_ephemeral
-cs_condor_configure
-cs_setup_etc_hosts
+if [ $UPDATE_CS == true ]; then
+    cs_condor_configure
+    cs_setup_etc_hosts
+fi
