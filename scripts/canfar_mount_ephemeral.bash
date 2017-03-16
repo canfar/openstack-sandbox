3745589f4c43f9a45c527edb4b338f372300325e
Sébastien Fabbro <sebfabbro@gmail.com>
2017-03-16 13:36:20 -0700

    remove some unecessary failures

--- scripts/canfar_mount_ephemeral.bash
+++ scripts/canfar_mount_ephemeral.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EXEC_NAME=$(basename $0 .${0##*.})
-EXEC_VERSION=0.1_beta
+EXEC_VERSION=0.1
 OLDMOUNTPOINT="/mnt"
 NEWMOUNTPOINT="/ephemeral"
 
@@ -15,9 +15,9 @@ die() {
 }
 
 grep -q ephemeral /etc/mtab && msg "$NEWMOUNTPOINT already mounted" && exit 0
-umount $OLDMOUNTPOINT >& /dev/null || msg "Could not unmount $OLDMOUNTPOINT"
-mkdir -p $NEWMOUNTPOINT >& /dev/null || msg "Could not create new mountpoint $NEWMOUNTPOINT"
-sed -i 's/\/mnt/\/ephemeral/g' /etc/fstab >& /dev/null || msg "Could not modify /etc/fstab"
+umount $OLDMOUNTPOINT >& /dev/null
+mkdir -p $NEWMOUNTPOINT >& /dev/null
+sed -i -e 's/\/mnt/\/ephemeral/g' /etc/fstab >& /dev/null
 mount $NEWMOUNTPOINT >& /dev/null || die "Could not mount $NEWMOUNTPOINT"
 chmod 777 $NEWMOUNTPOINT || msg "Could not set $NEWMOUNTPOINT permissions"
 
